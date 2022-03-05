af29fd8d66189e7151dec7adf1e5e2f66a1187da
Josué Enriquez <elcolornoloestodo@gmail.com>
2022-03-05 18:09:33 -0500

    :bug: Corregir rutas de archivos .css y .js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
 //?---------------Store---------------
 
-//!---------------Store Home---------------
+//!---------------St-Home---------------
 if (
   window.location.pathname === "/index.html" ||
   window.location.pathname === "AhuaccMaki-Ecommerce"
 ) {
+}
+
+//!---------------St-Home---------------
+if (
+  window.location.pathname === "/views/dashboard/Db-Home.html" ||
+  window.location.pathname === "/AhuaccMaki-Ecommerce/views/dashboard/Db-Home.html" 
+) {
+  
 }
